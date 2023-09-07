2ed2d1844b3da3620bfdb4df9238eb649ea32487
Huynh-Bao-Toan <toanit2382002@gmail.com>
2023-09-07 18:42:45 +0700

    code product rating

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
+import ProductRating from '~/components/ProductRating'
 import { Product as ProductType } from '~/types/product.type'
 import { formatNumberToSocial, formatPrice } from '~/utils/formatPrice'
 
 function Product(props: ProductType) {
-  const { image, name, price, price_before_discount, sold } = props
+  const { image, name, price, price_before_discount, sold, rating } = props
   return (
     <Link to='/'>
       <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
@@ -23,40 +24,7 @@ function Product(props: ProductType) {
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                  <svg
-                    enableBackground='new 0 0 15 15'
-                    viewBox='0 0 15 15'
-                    x={0}
-                    y={0}
-                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
-                  >
-                    <polygon
-                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeMiterlimit={10}
-                    />
-                  </svg>
-                </div>
-                <svg
-                  enableBackground='new 0 0 15 15'
-                  viewBox='0 0 15 15'
-                  x={0}
-                  y={0}
-                  className='w-3 h-3 fill-current text-gray-300'
-                >
-                  <polygon
-                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeMiterlimit={10}
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={rating} />
             <div className='ml-2 text-sm'>
               <span>{formatNumberToSocial(sold)}</span>
               <span className='ml-1'>Đã bán</span>
